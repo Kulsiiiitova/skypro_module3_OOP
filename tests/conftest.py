82b35ca4b61fe8fc7ba6bd4caf298c4b14d3450f
Kulsitova <kulsitovakarina@gmail.com>
2025-06-22 19:09:12 +0300

    add tack a homework14.1

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+
+@pytest.fixture()
+def product():
+    return Product(
+        name="Samsung Galaxy C23 Ultra",
+        description="256GB, Серый цвет, 200MP камера",
+        price=180000.0,
+        quantity=5,
+    )
+
+
+@pytest.fixture()
+def category():
+    return Category(
+        name="Телевизоры",
+        description="Современный телевизор, который позволяет наслаждаться просмотром, "
+        "станет вашим другом и помощником",
+        products=[
+            Product(
+                name='55" QLED 4K',
+                description="Фоновая подсветка",
+                price=123000.0,
+                quantity=7,
+            ),
+            Product(
+                name="Iphone 15",
+                description="512GB, Gray space",
+                price=210000.0,
+                quantity=8,
+            ),
+        ],
+    )
